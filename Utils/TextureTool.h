7768b2a314c37fe1fbc7163ead48bbc447bd22bb
cpvc <cpvc@openengine.dk>
2010-04-04 19:22:19 +0200

    updated texture tool to use new texture constructors
    darcs-hash:20100404172219-3a0eb-7332944fb6db560e55bef2400d07496200f11108.gz

--- Utils/TextureTool.h
+++ Utils/TextureTool.h
@@ -7,17 +7,55 @@
 using namespace OpenEngine::Resources;
 
 #include <FreeImage.h>
+#include <limits>
 
 class TextureTool {
  public:
-    static void DumpTexture(ITexture2DPtr tex, std::string filename) {
+    static void DumpTexture(FloatTexture2DPtr tex, std::string filename) {
         FreeImage_Initialise();
         //cout << "FreeImage vesrion: " << FreeImage_GetVersion() << endl;
         //cout << FreeImage_GetCopyrightMessage() << endl << endl;
 
         const unsigned int width = tex->GetWidth();
         const unsigned int height = tex->GetHeight();
-        const unsigned int channels = tex->GetChannels(); 
+        float* data = tex->GetData();
+
+        //const unsigned int channels = tex->GetChannels(); 
+        //logger.info << width << "x" << height << ":" << channels << logger.end;
+
+        // since we are outputting thre 8 bit RGB values
+        const unsigned int bpp = 1*sizeof(float)*8; 
+        //logger.info << "bpp: " << bpp << logger.end;
+        FIBITMAP* bitmap = FreeImage_AllocateT(FIT_FLOAT, width, height, bpp);
+        if (!bitmap)
+            throw Exception("Error: allocation failed");
+
+        // Draws a gradient from blue to green:
+        int bytespp = FreeImage_GetLine(bitmap) / FreeImage_GetWidth(bitmap);        
+        for(unsigned int y = 0; y < FreeImage_GetHeight(bitmap); y++) { 
+            BYTE* bits = (BYTE*)FreeImage_GetScanLine(bitmap, y);
+            for(unsigned int x = 0; x < FreeImage_GetWidth(bitmap); x++) {
+                bits[FI_RGBA_RED] = 255;
+                bits[FI_RGBA_GREEN] = 255;
+                bits[FI_RGBA_BLUE] = 255;
+                bits[FI_RGBA_ALPHA] = data[x+y*width];
+                bits += bytespp;
+            }
+        }
+        if ( !FreeImage_Save(FIF_EXR, bitmap, filename.c_str(), EXR_FLOAT) )
+          throw Exception("Failed to save image: " + filename);
+        FreeImage_Unload(bitmap);
+        FreeImage_DeInitialise();
+    }
+
+    static void DumpTexture(UCharTexture2DPtr tex, std::string filename) {
+        FreeImage_Initialise();
+        //cout << "FreeImage vesrion: " << FreeImage_GetVersion() << endl;
+        //cout << FreeImage_GetCopyrightMessage() << endl << endl;
+
+        const unsigned int width = tex->GetWidth();
+        const unsigned int height = tex->GetHeight();
+        //const unsigned int channels = tex->GetChannels(); 
 
         if(tex->GetType() != Types::UBYTE)
            throw Exception("not a byte texture");
@@ -26,16 +64,32 @@ class TextureTool {
         //for (unsigned int i = 0; i<height*width; i++)
         //  data[i] = 255;
 
-        logger.info << width << "x" << height << ":" << channels << logger.end;
+        //logger.info << width << "x" << height << ":" << channels << logger.end;
 
         FIBITMAP* bitmap = NULL;
 
         // since we are outputting thre 8 bit RGB values
-        const unsigned int bpp = 24; 
+        const unsigned int bpp = 32; 
         bitmap = FreeImage_Allocate(width, height, bpp);
         if (!bitmap)
             throw Exception("Error: allocation failed");
+        //bitmap = FreeImage_ConvertTo32Bits(bitmap);
 
+        // Calculate the number of bytes per pixel (3 for 24-bit or 4 for 32-bit) 
+        int bytespp = FreeImage_GetLine(bitmap) / FreeImage_GetWidth(bitmap);        
+        for(unsigned int y = 0; y < FreeImage_GetHeight(bitmap); y++) { 
+            BYTE* bits = (BYTE*)FreeImage_GetScanLine(bitmap, y);
+            for(unsigned int x = 0; x < FreeImage_GetWidth(bitmap); x++) {
+                bits[FI_RGBA_RED] = numeric_limits<float>::max();
+                bits[FI_RGBA_GREEN] = numeric_limits<float>::max();
+                bits[FI_RGBA_BLUE] = numeric_limits<float>::max();
+                bits[FI_RGBA_ALPHA] = data[x+y*width];
+
+                // jump to next pixel
+                bits += bytespp;
+            }
+        }
+        /*
         // Draws a gradient from blue to green:
         RGBQUAD color;
         for (unsigned int i=0 ; i < width; i++) {
@@ -44,12 +98,12 @@ class TextureTool {
                 color.rgbRed = pixel;
                 color.rgbGreen = pixel;
                 color.rgbBlue = pixel;
-                FreeImage_SetPixelColor(bitmap, i, height-j, &color);
+                FreeImage_SetPixelColor(bitmap, i, height-1-j, &color);
                 // Notice how we're calling the & operator on color
                 // so that we can pass a pointer to the color struct.
             }
         }
-
+        */
         /*
         int pitch = ((((depth * width) + 31) / 32) * 4);
         bitmap = 
@@ -61,8 +115,10 @@ class TextureTool {
             throw Exception("Error: allocation failed");
         */
 
-        if ( !FreeImage_Save(FIF_PNG, bitmap, filename.c_str(), 0) )
+        //if ( !FreeImage_Save(FIF_PNG, bitmap, filename.c_str(), PNG_Z_BEST_COMPRESSION) )
+        if ( !FreeImage_Save(FIF_PNG, bitmap, filename.c_str(), PNG_Z_BEST_SPEED) )
             throw Exception("Failed to save image: " + filename);
+        FreeImage_Unload(bitmap);
         FreeImage_DeInitialise();
     }
 };
