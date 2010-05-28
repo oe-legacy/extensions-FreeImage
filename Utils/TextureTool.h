3f80483e9ed96826f7558eee3c682a6a575fe379
cpvc <cpvc@openengine.dk>
2010-05-28 14:43:24 +0200

    texture tool now templated
    darcs-hash:20100528124324-3a0eb-70fdb2ceb59e32f1d0eee8877a62054a32d11833.gz

--- Utils/TextureTool.h
+++ Utils/TextureTool.h
@@ -21,19 +21,20 @@ using namespace OpenEngine::Resources;
 #define PNG_Z_BEST_COMPRESSION 0x0009  // save using ZLib level 9 compression flag (default value is 6)
 #endif
 
+template <class T>
 class TextureTool {
  private:
-static vector<FloatTexture2DPtr> ToLayers(FloatTexture3DPtr tex) {
+    static vector<Texture2DPtr(T)> ToLayers(Texture3DPtr(T) tex) {
     const unsigned int w = tex->GetWidth();
     const unsigned int h = tex->GetHeight();
     const unsigned int d = tex->GetDepth();
     const unsigned int c = tex->GetChannels();
-    const float* din = tex->GetData();
+    const T* din = tex->GetData();
 
-    vector<FloatTexture2DPtr> list;
+    vector<Texture2DPtr(T)> list;
     for (unsigned int z=0; z<d; z++) {
-        FloatTexture2DPtr output(new FloatTexture2D(w,h,c));
-        float* dout = output->GetData();
+        typename Texture2DPtr(T) output(new Texture2D<T>(w,h,c));
+        T* dout = output->GetData();
         for (unsigned int y=0; y<h; y++) {
             for (unsigned int x=0; x<w; x++) {
                 for (unsigned int ch=0; ch<c; ch++) {
@@ -47,7 +48,7 @@ static vector<FloatTexture2DPtr> ToLayers(FloatTexture3DPtr tex) {
 }
 
  public:
-    static void DumpTexture(FloatTexture2DPtr tex, std::string filename) {
+ static void DumpTexture(Texture2DPtr(T) tex, std::string filename) {
         FreeImage_Initialise();
 
         const unsigned int w = tex->GetWidth();
@@ -59,15 +60,25 @@ static vector<FloatTexture2DPtr> ToLayers(FloatTexture3DPtr tex) {
         logger.info << "saving: " << w << "x" << h << ":" << c 
                     << "-" << bpp << logger.end;
         */
-        FIBITMAP* bitmap = FreeImage_AllocateT(FIT_RGBAF, w, h, bpp);
+        FIBITMAP* bitmap = NULL;
+        if (typeid(T) ==typeid(float)) {
+            bitmap = FreeImage_AllocateT(FIT_RGBAF, w, h, bpp);
+        } else if (typeid(T) ==typeid(unsigned char)) {
+            bitmap = FreeImage_Allocate(w, h, bpp);
+        } else
+            throw Exception("unsupported texture dump format on alloc.");
+
         if (!bitmap)
             throw Exception("Error: allocation failed");
 
-        float* data = tex->GetData();
+        unsigned int bytespp = FreeImage_GetLine(bitmap) / w;
+        T* data = tex->GetData();
         for (unsigned int y = 0; y < h; y++) { 
-            float* bits = reinterpret_cast<float*>
+            T* bits = reinterpret_cast<T*>
                 (FreeImage_GetScanLine(bitmap,y));
             for (unsigned int x = 0; x < w; x++) {
+
+        if (typeid(T) ==typeid(float)) {
                 /*
                 float scale = 1.0f;
                 bits[x*4+0] = 1.0 * scale;
@@ -78,29 +89,47 @@ static vector<FloatTexture2DPtr> ToLayers(FloatTexture3DPtr tex) {
                 for (unsigned int ch = 0; ch < c; ch++) {
                     bits[x*c+ch] = data[(x+y*w)*c+ch];
                 }
+        } else if (typeid(T) ==typeid(unsigned char)) {
+                bits[FI_RGBA_BLUE]   = data[(x+y*w)*c + 0];
+                if (c >= 2)
+                bits[FI_RGBA_GREEN] = data[(x+y*w)*c + 1];
+                if (c >= 3)
+                bits[FI_RGBA_RED]  = data[(x+y*w)*c + 2];
+                if (c >= 4)
+                bits[FI_RGBA_ALPHA] = data[(x+y*w)*c + 3];                
+
+                // jump to next pixel
+                bits += bytespp;
+        }
             }
         }
 
-        if ( !FreeImage_Save(FIF_EXR, bitmap, filename.c_str(), EXR_FLOAT) )
-          throw Exception("Failed to save image: " + filename);
+        if (typeid(T) ==typeid(float)) {
+            if ( !FreeImage_Save(FIF_EXR, bitmap, filename.c_str(), EXR_FLOAT) )
+                throw Exception("Failed to save image: " + filename);
+        } else if (typeid(T) ==typeid(unsigned char)) {
+            if ( !FreeImage_Save(FIF_PNG, bitmap, filename.c_str(),
+                                 PNG_Z_BEST_COMPRESSION) )
+                                 //PNG_Z_BEST_SPEED) )
+                throw Exception("Failed to save image: " + filename);
+        } else
+            throw Exception("unsupported texture dump format.");
+
         FreeImage_Unload(bitmap);
         FreeImage_DeInitialise();
     }
 
-    static void DumpTexture(FloatTexture3DPtr input, std::string foldername) {
+    static void DumpTexture(Texture3DPtr(T) input, std::string foldername) {
         if (!Directory::Exists(foldername)) {
-            logger.info << "mkdir: " << foldername << logger.end;
             Directory::Make(foldername);
-        } else {
-            logger.info << "dir allready exists: " << foldername << logger.end;
         }
 
-        vector<FloatTexture2DPtr> texList = ToLayers(input);
+        vector<Texture2DPtr(T) > texList = ToLayers(input);
 
         int count = 1;
-        vector<FloatTexture2DPtr >::iterator itr = texList.begin();
+        typename vector<Texture2DPtr(T) >::iterator itr = texList.begin();
         while (itr != texList.end()) {
-            FloatTexture2DPtr tex = *itr;
+            Texture2DPtr(T) tex = *itr;
             std::string filename = foldername + "/";
 
             // @todo: make this general
@@ -108,13 +137,22 @@ static vector<FloatTexture2DPtr> ToLayers(FloatTexture3DPtr tex) {
             if (count<100) filename += "0";
 
             filename += Convert::ToString(count);
-            TextureTool::DumpTexture(tex, filename + ".exr");
+
+            std::string fileExtension = ".";
+            if (typeid(T) ==typeid(float)) {
+                fileExtension += "exr";
+            } else if (typeid(T) ==typeid(unsigned char)) {
+                fileExtension += "png";
+            } else
+                throw Exception("unsupported texture dump format.");
+
+            TextureTool<T>::DumpTexture(tex, filename + fileExtension);
 
             count++;
             ++itr;
         }
     }
-
+    /*
     static void DumpTexture(UCharTexture2DPtr tex, std::string filename) {
         FreeImage_Initialise();
         //cout << "FreeImage vesrion: " << FreeImage_GetVersion() << endl;
@@ -150,31 +188,30 @@ static vector<FloatTexture2DPtr> ToLayers(FloatTexture3DPtr tex) {
             }
         }
 
-        /*
-        // Draws a gradient from blue to green:
-        RGBQUAD color;
-        for (unsigned int i=0 ; i < width; i++) {
-            for (unsigned int j=0; j < height; j++) {
-                unsigned char pixel = data[i+j*width];
-                color.rgbRed = pixel;
-                color.rgbGreen = pixel;
-                color.rgbBlue = pixel;
-                FreeImage_SetPixelColor(bitmap, i, height-1-j, &color);
-                // Notice how we're calling the & operator on color
-                // so that we can pass a pointer to the color struct.
-            }
-        }
-        */
-        /*
-        int pitch = ((((depth * width) + 31) / 32) * 4);
-        bitmap = 
-            FreeImage_ConvertFromRawBits(data, width, height, pitch, depth,
-                                         FI_RGBA_RED_MASK, 
-                                         FI_RGBA_GREEN_MASK, 
-                                         FI_RGBA_BLUE_MASK);
-        if (!bitmap)
-            throw Exception("Error: allocation failed");
-        */
+
+//         // Draws a gradient from blue to green:
+//         RGBQUAD color;
+//         for (unsigned int i=0 ; i < width; i++) {
+//             for (unsigned int j=0; j < height; j++) {
+//                 unsigned char pixel = data[i+j*width];
+//                 color.rgbRed = pixel;
+//                 color.rgbGreen = pixel;
+//                 color.rgbBlue = pixel;
+//                 FreeImage_SetPixelColor(bitmap, i, height-1-j, &color);
+//                 // Notice how we're calling the & operator on color
+//                 // so that we can pass a pointer to the color struct.
+//             }
+//         }
+
+//         int pitch = ((((depth * width) + 31) / 32) * 4);
+//         bitmap = 
+//             FreeImage_ConvertFromRawBits(data, width, height, pitch, depth,
+//                                          FI_RGBA_RED_MASK, 
+//                                          FI_RGBA_GREEN_MASK, 
+//                                          FI_RGBA_BLUE_MASK);
+//         if (!bitmap)
+//             throw Exception("Error: allocation failed");
+
 
         //if ( !FreeImage_Save(FIF_PNG, bitmap, filename.c_str(), PNG_Z_BEST_COMPRESSION) )
         if ( !FreeImage_Save(FIF_PNG, bitmap, filename.c_str(), PNG_Z_BEST_SPEED) )
@@ -182,6 +219,7 @@ static vector<FloatTexture2DPtr> ToLayers(FloatTexture3DPtr tex) {
         FreeImage_Unload(bitmap);
         FreeImage_DeInitialise();
     }
+*/
 };
 
 #endif // _TEXTURE_TOOL_
